d487d1bfb0b4c7e5888bc526b2669f5b88be2710
Rian Mendonça <rianmendonca@proton.me>
2024-03-21 15:55:38 -0300

    Ajuste no método do endpoint

--- src/main/java/com/springboot/restful/api/resources/UserResource.java
+++ src/main/java/com/springboot/restful/api/resources/UserResource.java
@@ -1,21 +1,25 @@
 package com.springboot.restful.api.resources;
 
 import com.springboot.restful.api.domain.User;
+import com.springboot.restful.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        List<User> usersList = new ArrayList<>();
+        List<User> usersList = userService.findAll();
         return ResponseEntity.ok().body(usersList);
     }
 
