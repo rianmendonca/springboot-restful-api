78a64e2f4bc282bac855d80115d429cbea6d1e31
Rian Mendonça <rianmendonca@proton.me>
2024-03-22 18:48:33 -0300

    add método para atualizar usuário

--- src/main/java/com/springboot/restful/api/resources/UserResource.java
+++ src/main/java/com/springboot/restful/api/resources/UserResource.java
@@ -46,4 +46,12 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id) {
+        User user = userService.fromDTO(userDTO);
+        user.setId(id);
+        user = userService.update(user);
+        return ResponseEntity.noContent().build();
+    }
+
 }
