d4ef2f753fb225a1447f29a356cb6b20a848ab92
Rian Mendonça <rianmendonca@proton.me>
2024-03-27 14:36:15 -0300

    fixing

--- src/main/java/com/springboot/restful/api/domain/Post.java
+++ src/main/java/com/springboot/restful/api/domain/Post.java
@@ -1,27 +1,35 @@
 package com.springboot.restful.api.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+@Document
 public class Post implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
+
+    @Id
     private String id;
     private Date date;
     private String title;
     private String body;
+    private User author;
 
     public Post() {
     }
 
-    public Post(String id, Date date, String title, String body) {
+    public Post(String id, Date date, String title, String body, User author) {
         this.id = id;
         this.date = date;
         this.title = title;
         this.body = body;
+        this.author = author;
     }
 
     public String getId() {
@@ -54,6 +62,14 @@ public class Post implements Serializable {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 
     @Override
