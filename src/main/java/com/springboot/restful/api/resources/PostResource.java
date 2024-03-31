8c3db69ddef021fa6a051e93644386c39dfb08eb
Rian Mendonça <rianmendonca@proton.me>
2024-03-31 20:31:43 -0300

    add consulta de posts composta com query methods

--- src/main/java/com/springboot/restful/api/resources/PostResource.java
+++ src/main/java/com/springboot/restful/api/resources/PostResource.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -26,6 +27,17 @@ public class PostResource {
     public ResponseEntity<List<Post>> finByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
         text = URL.decodeParam(text);
         List<Post> postsList = postService.findByTitle(text);
+        return ResponseEntity.ok().body(postsList);
+    }
+
+    @GetMapping("/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+                                                 @RequestParam(value = "minDate", defaultValue = "") String minDate,
+                                                 @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0));
+        Date max = URL.convertDate(maxDate, new Date(0));
+        List<Post> postsList = postService.fullSearch(text, min, max);
         return ResponseEntity.ok().body(postsList);
     }
 
